47518e749f26ada1955f4ea971c659b806ca7a2c
Stephan Hagel <sthagel@uw.edu>
2018-10-22 21:17:37 -0700

    Calculating Kloas ambiguity and the scaled one. Still needs some work and cleaning up

--- main.py
+++ main.py
@@ -55,8 +55,8 @@ def thumb_sigma_y(size=DEFAULT_SIZE_Y):
 
 
 def cartesian_generic_probdist(x, y, size_x=DEFAULT_SIZE_X, size_y=DEFAULT_SIZE_Y):
-    s_x = generic_sigma_x(size_x)
-    s_y = generic_sigma_y(size_y)
+    s_x = 1.5 * generic_sigma_x(size_x)
+    s_y = 1.5 * generic_sigma_y(size_y)
     return np.exp(-0.5 * (x / s_x) ** 2 - 0.5 * (y / s_y) ** 2) / (2.0 * np.pi * s_x * s_y)
 
 
@@ -76,9 +76,14 @@ def polar_generic_probdist(r, phi, size_x=DEFAULT_SIZE_X, size_y=DEFAULT_SIZE_Y)
     return r * cartesian_generic_probdist(r * np.cos(phi), r * np.sin(phi), size_x, size_y)
 
 
-def int_probdist(r_low, r_up):
+def int_probdist_26(r_low, r_up):
     return dblquad(polar_generic_probdist, 0.0, 2.0 * np.pi,
                    lambda r: r_low, lambda r: r_up, args=(size_x_qwerty, size_y_qwerty))[0]
+
+
+def int_probdist_kloa(r_low, r_up):
+    return dblquad(polar_generic_probdist, 0.0, 2.0 * np.pi,
+                   lambda r: r_low, lambda r: r_up, args=(size_x_kloa, size_y_kloa))[0]
 # These functions return the probability distruibution at the point (x,y) on a button of a given size under the
 # assumption that the sigmas in x an dy direction are not correlated and with mu_x and mu_y set to zero.
 
@@ -93,32 +98,75 @@ def int_probdist(r_low, r_up):
 #                             lambda x: -size_x_kloa / 2.0,
 #                             lambda x: size_x_kloa / 2.0, args=(size_x_kloa, size_y_kloa))[0]
 
-ratio = 8.0 / 7.0
+ratio_26 = 8.0 / 7.0
 
-steps = np.arange(0.001, 20.01, 0.01)
-ambi = []
+steps = np.arange(1.1, 15.0, 0.1)
+
+ambi_26 = []
+
 for i in steps:
     size_x_qwerty = i
-    size_y_qwerty = ratio * size_x_qwerty
+    size_y_qwerty = ratio_26 * size_x_qwerty
     r1 = size_x_qwerty / 2.0
     r2 = size_y_qwerty / 2.0
     r3 = np.sqrt(size_x_qwerty ** 2 + size_y_qwerty ** 2) / 2.0
     r4 = np.sqrt(size_x_qwerty ** 2 + (size_y_qwerty / 2.0) ** 2)
     r5 = 1.5 * size_x_qwerty
     r6 = 1.5 * size_y_qwerty
-# These are the distances from the center of the g-Button on a 26 letter keyboard to:
-#  - f and h (r1)
-#  - v, t and y (r2)
-#  - c and b (r3)
-#  - r and u (r4)
-#  - d and j (r5)
-#  - the upper end of the keyboard / the spacebar (r6)
-    ambi.append(1.0 * int_probdist(0, r1) + 3.0 * int_probdist(r1, r2) + 6.0 * int_probdist(r2, r3)
-                + 8.0 * int_probdist(r3, r4) + 10.0 * int_probdist(r4, r5) + 12.0 * int_probdist(r5, r6))
-# We need to sum up all ambiguities and their respective probabilities
+    # These are the distances from the center of the g-Button on a 26 letter keyboard to:
+    #  - f and h (r1)
+    #  - v, t and y (r2)
+    #  - c and b (r3)
+    #  - r and u (r4)
+    #  - d and j (r5)
+    #  - the upper end of the keyboard / the spacebar (r6)
+    ambi_26.append(1.0 * int_probdist_26(0, r1) + 3.0 * int_probdist_26(r1, r2) + 6.0 * int_probdist_26(r2, r3)
+                   + 8.0 * int_probdist_26(r3, r4) + 10.0 * int_probdist_26(r4, r5) + 12.0 * int_probdist_26(r5, r6))
+    # We need to sum up all ambiguities and their respective probabilities
+
+ratio_kloa = 1.0
+
+ambi_kloa = []
+
+#for i in steps:
+#    size_x_kloa = i
+#    size_y_kloa = ratio_kloa * size_x_kloa
+#    r1 = size_x_kloa / 2.0
+#    r2 = size_x_kloa / np.sqrt(2.0)
+#    r3 = size_x_kloa * 1.0909
+    # Assuming a square-shape of the buttons, the calculation simplifies a lot! r3 is chosen to be comparable with r6 of
+    # the calculation above.
+#    ambi_kloa.append(3.0 * int_probdist_kloa(0, r1) + 10.0 * int_probdist_kloa(r1, r2)
+#                     + 14.0 * int_probdist_kloa(r2, r3))
+    # Here we assume to calculate the ambiguity of the letter e with an intrinsic ambiguity of 3. When reaching the
+    # letter t we also take into account the letters q and v, leading to an ambiguity of 6. When reaching the other
+    # buttons with two letters on them, we don't take into account the blue letters (b, k, p, x).
+
+# But how much bigger are the buttons in Kloa compared to QWERTY?
+
+scale = 2.0
+ambi_kloa_scaled = []
+
+for i in steps:
+    size_x_kloa = i * scale
+    size_y_kloa = ratio_kloa * size_x_kloa
+    r1 = size_x_kloa / 2.0
+    r2 = size_x_kloa / np.sqrt(2.0)
+    r3 = size_x_kloa * 1.0909
+    ambi_kloa_scaled.append(3.0 * int_probdist_kloa(0, r1) + 10.0 * int_probdist_kloa(r1, r2)
+                            + 14.0 * int_probdist_kloa(r2, r3))
+
+plt.ylabel('Ambiguity (QWERTY)')
+plt.xlabel('Button size, x dimension (mm)')
+plt.plot(steps, ambi_26)
+# plt.show()
+
+# plt.ylabel('Ambiguity')
+# plt.xlabel('Button size, x dimension (mm)')
+# plt.plot(steps, ambi_kloa)
+# plt.show()
 
 plt.ylabel('Ambiguity')
 plt.xlabel('Button size, x dimension (mm)')
-plt.plot(steps, ambi)
-
+plt.plot(steps, ambi_kloa_scaled)
 plt.show()
